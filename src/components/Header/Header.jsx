8857b413b6a40fc4db932df3cc27bf172fad3965
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-07-12 16:24:37 -0400

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './header.scss';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="header__logo" src="" alt="" />
+      <nav className="Header__nav">
+        <ul className="Header__nav-list">
+          <li className="Header__nav-list-item"></li>
+          <li className="Header__nav-list-item"></li>
+          <li className="Header__nav-list-item"></li>
+          <li className="Header__nav-list-item"></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
